ca1d5c14d2699c3a8205b26b3cefc371d2b7b3b9
Aleviannaf <alexandrevianna511@gmail.com>
2021-09-28 13:20:04 -0400

    FAlta substituir o shared preferences

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'models/item.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Carrinho',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = <Item>[];
+
+  HomePage({Key? key}) : super(key: key) {
+    items = [];
+    //items.add(Item(title: "Banana", done: false));
+    //items.add(Item(title: "Abacate", done: true));
+    //items.add(Item(title: "Melancia", done: false));
+  }
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+    setState(() {
+      widget.items.add(Item(title: newTaskCtrl.text, done: false));
+      newTaskCtrl.text = "";
+      save();
+    });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    
+    var data = prefs.getString('data');
+
+    if (data.isEmpty) {
+      Iterable decoded = jsonDecode(data);
+
+      Iterable result = decoded.map((x) => Item.fromJson(x)).cast().toList();
+
+      setState(() {
+        widget.items.addAll(result.cast());
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          decoration: const InputDecoration(
+              labelText: "Lista de Compra",
+              labelStyle: TextStyle(color: Colors.white)),
+        ),
+      ),
+      // ignore: avoid_unnecessary_containers
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+          return Dismissible(
+            key: Key(item.title.toString()),
+            child: CheckboxListTile(
+              title: Text(item.title.toString()),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                  save();
+                });
+              },
+            ),
+            background: Container(
+              color: Colors.red.withOpacity(0.2),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
+    );
+  }
+}
